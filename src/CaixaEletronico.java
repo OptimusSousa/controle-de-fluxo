cc7e83bf69a9d7fe56b36b96746575c8c69c14a0
sousa <mauriciocareca78@gmail.com>
2024-05-05 19:41:38 -0300

    update

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -2,11 +2,14 @@ public class CaixaEletronico {
     public static void main(String[] args) {
         
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 26.0;
 
-        if(valorSolicitado < saldo)
+        if(valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
-
+        }
+        else {
+            System.out.println("Você não possue saldo.");
+        }
         System.out.println(saldo);
     }
 }
